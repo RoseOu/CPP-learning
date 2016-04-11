23b63f3828715f4ec25671e63c86aba063660a75
RoseOu <598959770@qq.com>
2016-04-11 11:14:55 +0800

    C-language

---
+++ C-exercise/sixone.cpp
@@ -0,0 +1,21 @@
+//编程计算2+4+6+...+98+100的值。
+
+#include <stdio.h>
+#include <conio.h>
+
+void main()
+{
+	int sum,num;
+
+	sum = 0;
+	num = 2;
+
+	while (num <= 100)
+	{
+		sum = sum + num;
+		num = num + 2;
+	}
+
+	printf("sum:%d",sum);
+	getche();
+}
