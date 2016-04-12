5ca730d794ebaa7abdd4b3cc7cbc63bc683ab2ec
RoseOu <598959770@qq.com>
2016-04-12 18:57:39 +0800

    C-language

---
+++ C-exercise/sixtwo.cpp
@@ -0,0 +1,25 @@
+//编程计算1x2x3+3x4x5+...+99x100x101的值。
+
+#include <stdio.h>
+#include <conio.h>
+
+void main()
+{
+	int a,b,c,sum;
+
+	a = 1;
+	b = 2;
+	c = 3;
+	sum = 0;
+
+	while (a <= 99)
+	{
+		sum = sum + (a * b * c);
+		a = a + 1;
+		b = b + 1;
+		c = c + 1;
+	}
+
+	printf("The result:%d", sum);
+	getche();
+}
