f97ab96e64b55154ba91060c0569a4619b24cd90
RoseOu <598959770@qq.com>
2016-04-13 20:00:55 +0800

    C-labguage

---
+++ C-exercise/sixthree.cpp
@@ -0,0 +1,30 @@
+//编程计算1!+2!+...+10!的值。
+
+#include <stdio.h>
+#include <conio.h>
+
+void main()
+{
+	int i,sum,num,a;
+
+	i = 1;
+	a = 1;
+	num = 1;
+	sum = 0;
+
+	while (i <= 10)
+	{
+		while (a <= i)
+		{
+			num = num * a;
+			a = a + 1;
+		}
+		sum = sum + num;
+		num = 1;
+		i = i + 1;
+		a = 1;
+	}
+
+	printf ("The result is:%d", sum);
+	getche();
+}
